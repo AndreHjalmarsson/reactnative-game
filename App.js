df8410dc6740f09b4efdf8340dcf1a38c0ce7ff5
André Hjalmarsson <andre.hjalmarsson1@gmail.com>
2018-08-25 21:41:49 +0200

    initial commit

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Test from './src/Test';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Test />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'orange',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+});
---
+++ src/Test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class Test extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up Test.js to start working on your Test!</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'green',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%'
+  }
+});
